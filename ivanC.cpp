0e02fd7597cc2a8019e0f1b04b07df43ccb0831f
Ivan <42749125+TheIllusiveDeveloper@users.noreply.github.com>
2018-09-26 10:25:04 -0700

    Moved code from main repository to personal.

--- ivanC.cpp
+++ ivanC.cpp
@@ -2,9 +2,10 @@
 // This file will show my name in the credits for our project
 
 #include <iostream>
+#include "fonts.h"
 using namespace std;
 
-void show_credits()
+void ivanC(Rect x, int y)
 {
-    cout << "Ivan Cisneros";
+    ggprint8b(&x, 16, 0x00ff0000, "Ivan Cisneros");
 }
